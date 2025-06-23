548d5e5d90c59c6cabe38980595087cb1a5f975b
mollyqq <mollyqq@mail.ru>
2025-06-23 23:04:23 +0300

    изменил формулу

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ CHOICE_MAP = {
 
 # --- Старт бота ---
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    keyboard = ["приёмка", "упаковка"]
+    keyboard = [["приёмка", "упаковка"]]
     reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True, resize_keyboard=True)
     await update.message.reply_text(
         "Выберите тип работы:\n"
